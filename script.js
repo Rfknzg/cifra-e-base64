c5e8d04c63de5a1b5ffbaa29c920bac6d259f7e5
Rafiki <113565522+Rfknzg@users.noreply.github.com>
2022-10-19 21:43:36 -0300

    vfinale

--- script.js
+++ script.js
@@ -1,172 +1,135 @@
-window.onload = () => {
+//Criptografia
+var metodo = document.querySelector(".metodo");
+var passo = document.querySelector(".passo");
+var enviar = document.querySelector(".enviar");
+var tipoRadio = document.forms[0].coddecod;
+var tipo = document.querySelector(".tipo");
+var chave = document.getElementById("chave").value;
+var entrada = document.querySelector("#entrada");
 
-  document.getElementById('cypher').disabled = true;
-
-  document.getElementById('decypher').disabled = true;
-
-  document.getElementById('message-box').addEventListener('input', () => {
-
-    enableButtons(); 
-
-    addTextAreaRows();
-
-    clearResults();
-
-  });
-
-  document.getElementById('cypher').addEventListener('click', cypher);
-
-  document.getElementById('decypher').addEventListener('click', decypher);
-
-};
-
-const enableButtons = () => {
-
-  if (getMessage().trim().length !== 0) {
-
-    document.getElementById('cypher').disabled = false;
-
-    document.getElementById('decypher').disabled = false;
-
+passo.style.display = "none";
+metodo.addEventListener("change", function () {
+  metodo.style.backgroundColor = "#ffbb00";
+  metodo.style.transition = "none";
+  if (metodo.selectedIndex == 1) {
+    // "passo" aparece se Cifra for selecionado
+    passo.style.display = "flex";
   } else {
-
-    document.getElementById('cypher').disabled = true;
-
-    document.getElementById('decypher').disabled = true;
-
+    passo.style.display = "none";
   }
-
-};
-
-const addTextAreaRows = () => {
-
-  const txtArea = document.getElementById('message-box');
-
-  txtArea.style.height = '8vh';
-
-  txtArea.style.height = txtArea.scrollHeight + 'px';
-
-};
-
-const clearResults = () => {
-
-  if (document.getElementById('p-group')) 
-
-    document.getElementById('results').removeChild(document.getElementById('p-group'));
-
-};
-
-const cypher = () => {
-
-  const cypheredCharCodes = cypherCharcodes(getCharcodesFromMessage());
-
-  const resultMessage = transformIntoLetters(cypheredCharCodes);
-
-  const method = 'criptografada';
-
-  showResultOnPage(resultMessage, method);
-
-  clearTextArea();
-
-};
-
-const decypher = () => {
-
-  const decypheredCharCodes = decypherCharcodes(getCharcodesFromMessage());
-
-  const resultMessage = transformIntoLetters(decypheredCharCodes);
-
-  const method = 'descriptografada';
-
-  showResultOnPage(resultMessage, method);
-
-  clearTextArea();
-
-};
-
-const getMessage = () => document.getElementById('message-box').value;
-
-const getCharcodesFromMessage = () => getMessage().split('').map(val => val.charCodeAt());
-
-const cypherCharcodes = charcodesArray => charcodesArray.map((charCode, i) => {
-
-  if (charCode > 47 && charCode < 58) {
-
-    charCode = (charCode - 48 + 33) % 10 + 48;
-
-  } else if (charCode > 64 && charCode < 91) {
-
-    charCode = (charCode - 65 + 33) % 26 + 65;
-
-  } if (charCode > 96 && charCode < 192) {
-
-    charCode = (charCode - 97 + 33) % 26 + 97;
-
-  } else if (charCode > 191 && charCode < 222) {
-
-    charCode = (charCode - 192 + 33) % 30 + 192;
-
-  } else if (charCode > 221 && charCode <= 255) {
-
-    charCode = (charCode - 222 + 33) % 34 + 222;
-
-  } 
-
-  return charCode;
-
 });
 
-const decypherCharcodes = charcodesArray => charcodesArray.map((charCode, i) => {
-
-  if (charCode > 47 && charCode < 58) {
-
-    charCode = (charCode - 57 - 33) % 10 + 57;
-
-  } else if (charCode > 64 && charCode < 91) {
-
-    charCode = (charCode - 90 - 33) % 26 + 90;
-
-  } else if (charCode > 96 && charCode < 192) {
-
-    charCode = (charCode - 122 - 33) % 26 + 122;
-
-  } else if (charCode > 191 && charCode < 222) {
-
-    charCode = (charCode - 221 - 33) % 30 + 221;
-
-  } else if (charCode > 221 && charCode <= 255) {
-
-    charCode = (charCode - 255 - 33) % 34 + 255;
-
+tipo.addEventListener("change", function () {
+  // muda msg do botão de enviar, conforme opção selecionada
+  if (tipoRadio[0].checked) {
+    enviar.textContent = "Codificar Mensagem!";
+  } else {
+    enviar.textContent = "Decodificar Mensagem!";
   }
-
-  return charCode;
-
 });
 
-const transformIntoLetters = charcodesArray => String.fromCharCode.apply(this, charcodesArray);
+passo.addEventListener("change", function () {
+  // atualiza o valor da chave
+  chave = document.getElementById("chave").value;
+});
 
-const showResultOnPage = (resultMessage, method) => {
+enviar.addEventListener("click", function (e) {
+  e.preventDefault();
+  var mensagem = entrada.value;
+  if (metodo.selectedIndex == 0) {
+    metodo.style.backgroundColor = "orangered";
+    setTimeout(
+      () => (metodo.style.backgroundColor = "rgba(100, 25, 160, 0.7)"),
+      1000
+    );
+    metodo.style.transition = "1s";
+  } else if (metodo.selectedIndex == 1) {
+    if (tipoRadio[0].checked) {
+      codificarCifra(mensagem, chave);
+    } else {
+      decodificarCifra(mensagem, chave);
+    }
+  } else {
+    if (tipoRadio[0].checked) {
+      codificarBase64(mensagem);
+    } else {
+      decodificarBase64(mensagem);
+    }
+  }
+});
 
-  return document.getElementById('results').innerHTML = `
+// Base 64
+var resposta = document.querySelector(".resposta");
 
-  <div id="p-group">
+function codificarBase64(mensagem) {
+  var codigo = btoa(mensagem);
+  resposta.innerText = codigo;
+}
 
-    <p class="result-fixed-message w-100">Esta é a sua mensagem ${method}:</p>
+function decodificarBase64(codigo) {
+  var valido = true;
+  // para evitar Failed to execute 'atob'
+  for (var i = 0; i < codigo.length; i++) {
+    var charCode = codigo[i].charCodeAt();
+    if (
+      !(
+        (charCode >= 48 && charCode <= 57) ||
+        (charCode >= 65 && charCode <= 90) ||
+        (charCode >= 97 && charCode <= 122) ||
+        charCode == 61 ||
+        charCode == 43 ||
+        charCode == 47 ||
+        charCode == 32
+      )
+    ) {
+      valido = false;
+      tipo.style.backgroundColor = "orangered";
+      setTimeout(
+        () => (tipo.style.backgroundColor = "rgba(0, 0, 255, 0.7)"),
+        1000
+      );
+      tipo.style.transition = "1s";
+      break;
+    }
+  }
+  if (valido) {
+    var mensagem = atob(codigo);
+    resposta.innerText = mensagem;
+  }
+}
 
-    <p class="result-text w-100">${resultMessage}</p>
+// Cifra de César
+var resposta = document.querySelector(".resposta");
 
-  </div>`;
-
-};
-
-const clearTextArea = () => { 
-
-  document.getElementById('message-box').value = '';
-
-  enableButtons();
-
-  addTextAreaRows();
-
-};
-
+function codificarCifra(mensagem, chave) {
+  mensagem = mensagem.split("");
+  chave = parseInt(chave, 10);
+  var codigo = "";
+  for (var i = 0; i < mensagem.length; i++) {
+    var charCode = mensagem[i].charCodeAt();
+    if (charCode >= 65 && charCode <= 90) {
+      codigo += String.fromCharCode(((charCode + chave - 65) % 26) + 65);
+    } else if (charCode >= 97 && charCode <= 122) {
+      codigo += String.fromCharCode(((charCode + chave - 97) % 26) + 97);
+    } else {
+      codigo += mensagem[i];
+    }
+  }
+  resposta.innerText = codigo;
+}
+function decodificarCifra(mensagem, chave) {
+  mensagem = mensagem.split("");
+  chave = parseInt(chave, 10);
+  var codigo = "";
+  for (var i = 0; i < mensagem.length; i++) {
+    var charCode = mensagem[i].charCodeAt();
+    if (charCode >= 65 && charCode <= 90) {
+      codigo += String.fromCharCode(((charCode - chave - 90) % 26) + 90);
+    } else if (charCode >= 97 && charCode <= 122) {
+      codigo += String.fromCharCode(((charCode - chave - 122) % 26) + 122);
+    } else {
+      codigo += mensagem[i];
+    }
+  }
+  resposta.innerText = codigo;
+}
